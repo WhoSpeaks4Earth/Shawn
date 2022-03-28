ceca28d2310ee8e16e065ca4c09c9f4c5c475ec5
Shawn Bahramy <shawnbahramy@gmail.com>
2022-03-27 23:39:04 -0400

    basic banner and links to home page

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,7 +1,15 @@
+import { Box, Container, Flex, Image } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 export const Home = () => (
-    <div>home!<br/>
-        <Link to='/cardgame'>Card Game</Link>
-    </div>
+
+    <Container maxW='container.xl'>
+        <Image src='https://i1.sndcdn.com/visuals-000011092927-FHo4hC-t2480x520.jpg' alt='Banner' />
+        <Flex gap={4}>
+            <Box>Programming</Box>
+            <Box><Link to='/cardgame'>Card Game</Link></Box>
+            <Box>Music</Box>
+            <Box>Contact</Box>
+        </Flex>
+    </Container>
 )
